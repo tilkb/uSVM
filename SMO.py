fixed derivative with convexity issue

SMO.py
@@ -6,29 +6,27 @@ class SMO:
         self.labels=labels
         self.K=gram_matrix
         #no supportvector is a correct initialization
-        self.alfa= np.zeros(len(labels))        
+        self.alfa= np.zeros(len(labels))
 
     def calc_obj(self):
         label_array=self.labels.reshape(1,-1)
         alfa_array=self.alfa.reshape(1,-1)
         gram_label=np.dot(label_array.T,label_array)
         gram_alfa=np.dot(alfa_array.T,alfa_array)
-        np.multiply(gram_label,gram_alfa)
         return np.sum(self.alfa)-0.5*np.sum(np.multiply(self.K,np.multiply(gram_label,gram_alfa)))
 
     def search_max(self,nu,index1,index2):
+
         #remove index1 and index2
-        iter1=np.multiply(np.multiply(self.alfa,self.labels),self.K[index2,:])
+        iter1=np.multiply(np.multiply(self.alfa,self.labels),self.K[index1,:])
         iter1 = self.labels[index2]*(np.sum(iter1)-iter1[index1]-iter1[index2])
+        iter2=np.multiply(np.multiply(self.alfa,self.labels),self.K[index2,:])
+        iter2 = self.labels[index2]*(np.sum(iter2)-iter2[index1]-iter2[index2])
 
-        iter2=np.multiply(np.multiply(self.alfa,self.labels),self.K[index1,:])
-        iter2 = self.labels[index2]*(np.sum(iter1)-iter2[index1]-iter2[index2])
-    
-
-        result=-1.0/(2.0*self.K[index1,index2])*(1+nu*self.labels[index1]
-                -self.labels[index1]*self.labels[index2]-self.K[index1,index2]*self.labels[index2]*nu
-                -0.5*iter1-0.5*iter2)
-        return result
+        result= 1-self.labels[index1]*self.labels[index2] + self.K[index1,index1] * nu * self.labels[index2]-self.K[index1,index2]*self.labels[index2] * nu + 0.5 *iter1 - 0.5 * iter2 
+        
+        normalized_result =result / (self.K[index1,index1]+self.K[index2,index2] - 2*self.K[index1,index2])
+        return normalized_result 
                                                  
 
     def step(self, index1, index2):
@@ -53,34 +51,38 @@ class SMO:
             low = 0
 
         alfa2=self.search_max(nu,index1,index2)
-        print(alfa2)
-        print((nu-alfa2*self.labels[index2])*self.labels[index1])
         if alfa2>high:
             alfa2=high
         elif alfa2<low:
             alfa2=low
 
         #change variables
+        before = self.calc_obj()
+        tmp1=self.alfa[index2]
+        tmp2=self.alfa[index1]
         self.alfa[index2] = alfa2
         self.alfa[index1] = (nu-alfa2*self.labels[index2])*self.labels[index1]
+        #TODO: kitalalni miert nem monoton
+        if before>self.calc_obj():
+            self.alfa[index2]=tmp1
+            self.alfa[index1]=tmp2
             
         
-    def solve(self, tolerance=0.01, maxiter=1000):
+    def solve(self, tolerance=-0.01, maxiter=100):
         nr_iter=0
         #differance between iteraions
         diff=tolerance+1.0
         before=self.calc_obj()
         while nr_iter<maxiter and tolerance<diff:
             #select two item
-            index = np.random.permutation(len(self.alfa))
-            for i in range(len(self.alfa)//2):
-                self.step(index[i],index[-1-i]) 
+
+            for i in range(len(self.alfa)*2):
+                self.step(np.random.randint(0,len(self.alfa)),np.random.randint(0,len(self.alfa)))
 
             after=self.calc_obj()
-            diff=before-after
+            diff=after-before
             before=after
             nr_iter=nr_iter + 1
-            print(after)
 
         return self.alfa
         

test.py
@@ -2,9 +2,9 @@ import numpy as np
 from kernel import linear
 from SVM import SVM
 
-X=np.array([[1,1],[0,0.5],[3,3],[5,5]])
-Y=np.array([-1,-1,1,1])
+X=np.array([[0,1],[1,2],[1,3],[0,4]])
+Y=np.array([-1,-1,-1,1])
 
-svm=SVM()
+svm=SVM(C=1)
 
 svm.fit(X,Y)